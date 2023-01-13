584082a0aff450786147d92dc49006ff38ae30c6
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-12 16:37:27 -0800

    Add annotation

--- python_logging/log_comms.py
+++ python_logging/log_comms.py
@@ -1,13 +1,18 @@
 import serial
 
+# Read and write to file
 with open("index.html", 'w') as f:
-	with serial.Serial('/dev/cu.usbmodem1101', 9600, timeout=1) as ser:
-		while True:
-			s = ser.read(1)        # read up to ten bytes (timeout)
-			if s != b"":
-				print(s.decode("utf-8"), end="", flush=True)
-				f.write(s.decode("utf-8"))
+	with serial.Serial('/dev/cu.usbmodem101', 9600, timeout=1) as ser:
+		while True: # Infinite loop
+			serial_input = ser.read(1)        # read up to ten bytes (timeout)
+			if serial_input != b"":
+				print(serial_input.decode("utf-8"), end="", flush=True) # Print to terminal
+				f.write(serial_input.decode("utf-8")) # Write to file - decode
 
+
+
+
+# Read only
 # with serial.Serial('/dev/cu.usbmodem101', 9600, timeout=1) as ser:
 # 	while True:
 # 		s = ser.read(1)        # read up to ten bytes (timeout)
